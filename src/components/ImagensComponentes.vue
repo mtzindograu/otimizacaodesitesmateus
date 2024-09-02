eb90247a224e06b0270acefb002c528c2b061e23
mtzindograu <117742961+mtzindograu@users.noreply.github.com>
2024-09-02 10:06:07 -0300

    Add files via upload

--- src/components/ImagensComponentes.vue
+++ src/components/ImagensComponentes.vue
@@ -1,4 +1,6 @@
+
 <template>
+
 <div style="margin-left: 30%;">
     <div style="width: 60%;">
       <img src="/public/imagem/caramelo-meme.png" width="100%">
@@ -11,7 +13,27 @@
         <img src="/public/imagem/bolsonarololi.jpg" width="100%" height="100%">
       </div>
     </div>
+    <div style="display: flex;">
+    <div style="width: 30%;">
+        <img src="/public/imagem/imagemgrande1.jpg" width="100%" height="100%" loading="lazy">
     </div>
+    <div style="width: 30%;">
+        <img src="/public/imagem/imagemgrande2.jpg" width="100%" height="100%" loading="lazy">
+    </div>
+  </div>
+  <div style="display: flex;">
+    <div style="width: 30%;">
+        <img src="/public/imagem/imagemgrande3.jpg" width="100%" height="100%" loading="lazy">
+    </div>
+    <div style="width: 30%;">
+        <img src="/public/imagem/imagemgrande4.jpg" width="100%" height="100%" loading="lazy">
+    </div>
+  </div>
+    <div style="width: 60%;">
+        <img src="/public/imagem/imagemgrande5.jpg" width="100%" height="100%" loading="lazy">
+    </div>
+      </div>    
+    
 
 
 
